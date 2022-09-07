2754138c88a3d45c80d95bfbdf8ed1df9b615e2e
Andreza Mitterstein Prebianca <102162509+andr3za@users.noreply.github.com>
2022-09-07 20:20:00 -0300

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurante
+{
+    public class Menu
+    {
+       public int Sujo { get; set; }
+
+        public int Servir { get; set; }
+
+        public bool SairDoRestaurante()
+
+        {
+            if (Sujo == 0)
+            {
+                Console.WriteLine("Solite a limpeza do prato para finalizar");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Volte sempre!");
+                return true;
+            }
+        }
+        public bool LimparPrato()
+        {
+            if (Servir == 0)
+            {
+                Console.WriteLine("Antes de limpar o prato, faça o seu pedido");
+                return false;
+            }
+
+            else
+            {
+                Console.WriteLine("Prato Limpo");
+                return true;
+            }
+        }
+        
+
+    }
+
+        
+    
+}
+
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using restaurante;
+
+Console.WriteLine("Restaurante");
+Menu menu = new Menu();
+
+string opcao;
+bool menuAberto = true;
+menu.LimparPrato();
+int contador = 0;
+
+while (menuAberto)
+{
+    Console.Clear();
+
+    Console.WriteLine("Menu\n");
+    Console.WriteLine("1 - Servir prato");
+    Console.WriteLine("2 - Limpar prato");
+    Console.WriteLine("3 - Sair\n");
+
+    Console.WriteLine("Informe a opção desejada:\n");
+
+    opcao = Console.ReadLine();
+
+    if (opcao == "1")
+    {
+        Console.WriteLine("O prato solicitado será servido em breve");
+        contador = contador + 1;
+
+        Console.WriteLine("\nPressione uma tecla para continuar");
+        Console.ReadKey();
+
+    }
+
+    else if (opcao == "2")
+    {
+        menu.LimparPrato();
+        
+        Console.WriteLine("\nPressione uma tecla para continuar");
+        Console.ReadKey();
+
+    }
+    else if (opcao == "3")
+    {
+        menu.SairDoRestaurante();
+        Console.WriteLine($"Pratos pedidos: {contador}");
+        menuAberto = menu.SairDoRestaurante();
+
+    }
+
+    else
+    {
+        Console.WriteLine("Não existe opcao...\n");
+    }
+
+
+
+
+}
+
+    
